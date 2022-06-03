4913044f785580b5af246ae9a4a8d312dd5fb462
Dmitriy <prof3662@gmail.com>
2022-06-03 16:31:43 +0800

    mod and add JS

--- js/coment.js
+++ js/coment.js
@@ -1,48 +1,48 @@
 "use strict";
 
-// alert('Hello!');
+alert('Hello!');
 
-// const obj = {
-//     name: 'Diman',
-//     age: 34,
-//     isMarried: false
-// };
+const obj = {
+    name: 'Diman',
+    age: 34,
+    isMarried: false
+};
 
-// console.log(obj.name);
+console.log(obj.name);
 
-// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
-// console.log(arr[1]);
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
 
-// const result = confirm('Are you here?');
-// console.log(result);
+const result = confirm('Are you here?');
+console.log(result);
 
-// const answer = +prompt('Вам есть 18?', '18');
-// console.log(answer + 5);
+const answer = +prompt('Вам есть 18?', '18');
+console.log(answer + 5);
 
-// const answers = [];
+const answers = [];
 
-// answers[0] = prompt('Ваше имя?', '');
-// answers[1] = prompt('Ваша фамилия?', '');
-// answers[2] = prompt('Сколько вам лет?', '');
+answers[0] = prompt('Ваше имя?', '');
+answers[1] = prompt('Ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
 
-// console.log(typeof(answers));
+console.log(typeof(answers));
 
-// const category = 'toys';
+const category = 'toys';
 
-// console.log(`https://someurl.com/${category}/5`);
+console.log(`https://someurl.com/${category}/5`);
 
-// const user = 'Dmitriy';
+const user = 'Dmitriy';
 
-// alert(`Привет, ${user}`);
+alert(`Привет, ${user}`);
 
-// console.log('arr' + ' - object');
-// console.log(4 + '5');
+console.log('arr' + ' - object');
+console.log(4 + '5');
 
-// let incr = 10,
-//     decr = 10;
+let incr = 10,
+    decr = 10;
 
-// incr++;
-// decr--;
+incr++;
+decr--;
 
-// console.log(incr);
-// console.log(decr);
+console.log(incr);
+console.log(decr);
