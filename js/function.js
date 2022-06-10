3100866db7a5ec4d29bf086147fbc911e82b98ee
Dmitriy <prof3662@gmail.com>
2022-06-10 12:23:07 +0800

    add dintip.js

--- js/function.js
+++ js/function.js
@@ -38,3 +38,24 @@ const calc = (a, b) => {
     return a + b;
 };
 console.log(calc(2, 5));  //7
+
+function hello() {
+    console.log('Hello world!');
+}
+
+hello();
+
+function hi() {
+    console.log('Say hi!');
+}
+
+hi();
+
+const arr = [1, 14, 4, 30, 54],
+    sorted = arr.sort(compareNum);
+
+    function compareNum(a, b) {
+        return a - b;
+    }
+
+    console.log(sorted);
