c33d9edf17b5c2d81809295bc51e5512fdfd30b7
Dmitriy <prof3662@gmail.com>
2022-06-17 21:12:12 +0800

    mod classes.js

--- js/classes.js
+++ js/classes.js
@@ -38,12 +38,13 @@ console.log(long.calcArea());
 // Используем классы для карточек
 
 class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
         this.src = src;
         this.alt = alt;
         this.title = title;
         this.descr = descr;
         this.price = price;
+        this.classes = classes;
         this.parent = document.querySelector(parentSelector);
         this.transfer = 55;
         this.changeToRUB();
@@ -55,16 +56,21 @@ class MenuCard {
 
     render() {
         const element = document.createElement('div');
+        if (this.classes.length === 0) {
+            this.element = 'menu__item';
+            element.classList.add(this.element);
+        } else {
+            this.classes.forEach(className => element.classList.add(className));
+        }
+
         element.innerHTML = `
-            <div class="menu__item">
-                <img src=${this.src} alt=${this.alt}>
-                <h3 class="menu__item-subtitle">${this.title}</h3>
-                <div class="menu__item-descr">${this.descr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> Руб./день</div>
-                </div>
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> Руб./день</div>
             </div>
         `;
         this.parent.append(element);
@@ -86,7 +92,8 @@ new MenuCard(
     'Меню “Премиум”',
     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
     10,
-    '.menu .container'
+    '.menu .container',
+    'menu__item'
 ).render();
 
 new MenuCard(
@@ -95,5 +102,6 @@ new MenuCard(
     'Меню "Постное"',
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     9,
-    '.menu .container'
+    '.menu .container',
+    'menu__item'
 ).render();
