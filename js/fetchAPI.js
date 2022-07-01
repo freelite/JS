9f1a72ea21e4e35e70e1add9b038c1bf7d96cddf
Dmitriy <prof3662@gmail.com>
2022-07-01 11:04:03 +0800

    nod fetchAPI.js

--- js/fetchAPI.js
+++ js/fetchAPI.js
@@ -1,3 +1,5 @@
+// 1)
+
 fetch('https://jsonplaceholder.typicode.com/posts', {
     method: "POST",
     body: JSON.stringify({name: 'Diman'}),
@@ -7,3 +9,58 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
 })
 .then(response => response.json())
 .then(json => console.log(json));
+
+// 2)
+
+const forms = document.querySelectorAll('form');
+
+const message = {
+    loading: 'img/form/spinner.svg',
+    success: 'Спасибо! Мы скоро с Вами свяжемся',
+    failure: 'Что-то пошло не так...'
+};
+
+forms.forEach(item => {
+    postData(item);
+});
+
+function postData(form) {
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const statusMessage = document.createElement('img');
+        statusMessage.src = message.loading;
+        statusMessage.style.cssText = `
+            display: block;
+            margin: 0 auto;
+        `;
+        form.insertAdjacentElement('afterend', statusMessage);
+
+        const formData = new FormData(form);
+
+        const object = {};
+        formData.forEach(function(value, key) {
+            object[key] = value;
+        });
+
+        fetch('server.php', {
+            method: "POST",
+            headers: {
+                'Content-type': 'appliocation/json'
+            },
+            body: JSON.stringify(object)
+        })
+        .then(data => data.text())
+        .then(data => {
+            console.log(data);
+            showThanksModal(message.success);
+            statusMessage.remove();
+        })
+        .catch(() => {
+            showThanksModal(message.failure);
+        })
+        .finally(() => {
+            form.reset();
+        });
+    });
+}
